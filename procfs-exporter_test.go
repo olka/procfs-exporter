2c67e17e23ec0f3ce48b2fd6654024bde96f52d9
olka <alexander.kachur@gmail.com>
2018-02-20 21:41:24 +0200

    test fix

--- procfs-exporter_test.go
+++ procfs-exporter_test.go
@@ -18,7 +18,7 @@ func TestLoadAvg(t *testing.T) {
 	}
 }
 
-func TestGetServerPort(t *testing.T) {
+func TestServerPortGetter(t *testing.T) {
 	expectedResult := "42"
 	os.Setenv("SERVER_PORT", expectedResult)
 	actualResult := getServerPort()
@@ -31,7 +31,7 @@ func TestGetServerPort(t *testing.T) {
 	}
 }
 
-func TestHeader(t *testing.T) {
+func TestCSVHeader(t *testing.T) {
 	expectedResult := "load_1, load_5, load_10, procs_running, procs_idle, ctxt_switches, uptime, last_pid, blocked, sirq_hi, sirq_timer, sirq_NetTx, sirq_NetRx, sirq_block, sirq_block_io_poll, sirq_tasklet, sirq_scheduler, sirq_hr_timer, sirq_rcu, sirq_total"
 	actualResult := prepareCSVHeader()
 	if !strings.Contains(actualResult, expectedResult) {
@@ -44,7 +44,7 @@ func TestHeader(t *testing.T) {
 }
 
 func TestStat(t *testing.T) {
-	expectedResult := "2.93%, 0.81%, 96.03%, 0.16%, 0.00%, 0.07%, 2.98%, 0.86%, 96.01%, 0.16%, 0.00%, 0.00%, 2.89%, 0.76%, 96.05%, 0.17%, 0.00%, 0.13%, 8109530, 3416, 0, 4409058, 5, 478252, 2038, 1736808, 13786, 0, 476620, 432032, 0, 1269517"
+	expectedResult := "0 2.93%, 0.81%, 96.03%, 0.16%, 0.00%, 0.07%, 2.98%, 0.86%, 96.01%, 0.16%, 0.00%, 0.00%, 2.89%, 0.76%, 96.05%, 0.17%, 0.00%, 0.13%, 8109530, 3416, 0, 4409058, 5, 478252, 2038, 1736808, 13786, 0, 476620, 432032, 0, 1269517"
 	actualResult := parseStat("test/stat")
 	if expectedResult != actualResult {
 		t.Error(
